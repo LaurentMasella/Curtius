ebf2a7b60e9f5308d44c95403050c1f08b4d2c1c
UXYann <yann.cadoret@ux-republic.com>
2013-12-30 18:02:55 +0100

    added a minimal support of HTML5 History A.P.I

--- js/ajax-builder.js
+++ js/ajax-builder.js
@@ -1,3 +1,62 @@
+
+window.addEventListener("popstate", function(e) {
+    var toLoad = location.pathname + ' .content';
+			$('.content').hide('fast',loadContent);
+			$('#load').remove();
+			$('#wrapper').append('<span id="load">LOADING...</span>');
+			$('#load').fadeIn('normal');
+			//	Set the new value of the URL bar	
+			history.pushState(null, null, location.pathname);
+			
+			function loadContent() {
+				$('#wrapper').load(toLoad,'',showNewContent());
+			}
+
+			function showNewContent() {
+				$('.content').show('normal',hideLoader());
+			}
+
+			function hideLoader() {
+
+				$('#load').fadeOut('normal');
+
+				window.setTimeout(
+					function(){
+
+						if(location.pathname.indexOf('map.html') != -1) {
+						    populateMap1();
+						    displayingMap();
+						    window.setTimeout(addingSpotLights, 100);
+						    addingClicksFeatures();
+						    window.setTimeout(localize, 250);					
+						}
+						parsingLinks.init();
+					}
+				, 50);
+
+			}
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var s, 
 	parsingLinks = {
 
@@ -35,12 +94,14 @@ var s,
 			var currentId = whichOne.id;
 								  
 			var toLoad = $(whichOne).attr('href')+' .content';
+
+
 			$('.content').hide('fast',loadContent);
 			$('#load').remove();
 			$('#wrapper').append('<span id="load">LOADING...</span>');
 			$('#load').fadeIn('normal');
 			//	Set the new value of the URL bar	
-			window.location.hash = $(whichOne).attr('href').substr(0,$(whichOne).attr('href').length-5);
+			history.pushState(null, null, whichOne.href);
 			
 			function loadContent() {
 				$('#wrapper').load(toLoad,'',showNewContent());
