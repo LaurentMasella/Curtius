3fb9df2e58e912ef9934f1d222b6e6eb2e3b4a6e
Romain <rm.maury@gmail.com>
2014-01-30 17:51:50 +0100

    internationalization complete all text traduction success motherfucker

--- js/src/Internationalization.js
+++ js/src/Internationalization.js
@@ -3,190 +3,200 @@ function Internationalization(){
 }
 
 /* ---- PARCOURS ---- */
+Internationalization.chooseLangfr = 'Choisissez votre langue';
+Internationalization.chooseLangeng = 'Select your language';
+Internationalization.chooseLangned = '';
+Internationalization.chooseLangdeu = '';
+
+Internationalization.choicevisitfr = 'Choisissez votre visite';
+Internationalization.choicevisiteng = 'Select your tour';
+Internationalization.choicevisitned = 'Kies uw bezoek';
+Internationalization.choicevisitdeu = 'Wählen Sie Ihren Besichtigung';
+
 Internationalization.visitBtnFreefr = 'Visite libre';
-Internationalization.VisitBtnFreeeng = 'Free visit';
-Internationalization.VisitBtnFreened = '';
-Internationalization.VisitBtnFreedeu = ' - ';
+Internationalization.VisitBtnFreeeng = 'Self-guided tours';
+Internationalization.VisitBtnFreened = 'Vrij bezoek';
+Internationalization.VisitBtnFreedeu = 'Freie Besichtigung';
 //
 Internationalization.VisitBtnFastfr = 'Visite rapide';
-Internationalization.VisitBtnFasteng = 'Quick visit';
-Internationalization.VisitBtnFastned = '';
-Internationalization.VisitBtnFastdeu = '';
+Internationalization.VisitBtnFasteng = 'Quick tours';
+Internationalization.VisitBtnFastned = 'Snel bezoek';
+Internationalization.VisitBtnFastdeu = 'Schnelle Besichtigung';
 //
 Internationalization.VisitBtnPerfr = 'Visite par période';
-Internationalization.VisitBtnPereng = 'Visit by period';
-Internationalization.VisitBtnPerned = '';
-Internationalization.VisitBtnPerdeu = '';
+Internationalization.VisitBtnPereng = 'Tours by period';
+Internationalization.VisitBtnPerned = 'Bezoek per periode';
+Internationalization.VisitBtnPerdeu = 'Besichtigung pro Zeitraum';
 //
 Internationalization.VisitBtnScolfr = 'Visite scolaire';
-Internationalization.VisitBtnScoleng = 'School visit';
-Internationalization.VisitBtnScolned = '';
-Internationalization.VisitBtnScoldeu = '';
+Internationalization.VisitBtnScoleng = 'School tours';
+Internationalization.VisitBtnScolned = 'Schoolbezoek';
+Internationalization.VisitBtnScoldeu = 'Schulbesichtigung';
 
 /* ---- EXPLANATION ---- */
 //
 Internationalization.LifiExpTitlefr = 'Comment ça marche ?';
-Internationalization.LifiExpTitleeng = 'How it works ?';
-Internationalization.LifiExpTitlened = '';
-Internationalization.LifiExpTitledeu = '';
+Internationalization.LifiExpTitleeng = 'How does it work?';
+Internationalization.LifiExpTitlened = 'Hoe werkt het?';
+Internationalization.LifiExpTitledeu = 'Wie funktioniert das ?';
 //
 Internationalization.LifiExpSubTitlefr = 'C\'est facile !';
-Internationalization.LifiExpSubTitleeng = '';
-Internationalization.LifiExpSubTitlened = '';
-Internationalization.LifiExpSubTitledeu = '';
+Internationalization.LifiExpSubTitleeng = 'Easy!';
+Internationalization.LifiExpSubTitlened = 'Heel eenvoudig!';
+Internationalization.LifiExpSubTitledeu = 'So einfach !';
 //
 Internationalization.LifiExpStep1fr = 'Etape <span>1</span> :';
-Internationalization.LifiExpStep1eng = '';
-Internationalization.LifiExpStep1ned = '';
-Internationalization.LifiExpStep1deu = '';
+Internationalization.LifiExpStep1eng = 'Stage <span>1</span> :';
+Internationalization.LifiExpStep1ned = 'Stap <span>1</span> :';
+Internationalization.LifiExpStep1deu = 'Schritt <span>1</span> :';
 //
 Internationalization.LifiExpStep2fr = 'Etape <span>2</span> :';
-Internationalization.LifiExpStep2eng = '';
-Internationalization.LifiExpStep2ned = '';
-Internationalization.LifiExpStep2deu = '';
+Internationalization.LifiExpStep2eng = 'Stage <span>2</span> :';
+Internationalization.LifiExpStep2ned = 'Stap <span>2</span> :';
+Internationalization.LifiExpStep2deu = 'Schritt <span>2</span> :';
 //
 Internationalization.LifiTxt1fr = 'La technologie Lifi utilise la lumière<br/> pour transmettre des données en haut débit.';
-Internationalization.LifiTxt1eng = '';
-Internationalization.LifiTxt1ned = '';
-Internationalization.LifiTxt1deu = '';
+Internationalization.LifiTxt1eng = 'Li-Fi uses light to transmit data at high speed.';
+Internationalization.LifiTxt1ned = 'De Lifi-technologie maakt gebruik van het licht voor een datatransmissie op hoge snelheid.';
+Internationalization.LifiTxt1deu = 'Die Lifi-Technologie nutzt das Licht, um Daten im hohen Volumen zu versenden.';
 //
 Internationalization.LifiTxt2fr = 'Placez-vous sous une source lumineuse.';
-Internationalization.LifiTxt2eng = '';
-Internationalization.LifiTxt2ned = '';
-Internationalization.LifiTxt2deu = '';
+Internationalization.LifiTxt2eng = 'Go and stand under a light source.';
+Internationalization.LifiTxt2ned = 'Neem plaats onder een lichtbron.';
+Internationalization.LifiTxt2deu = 'Stellen Sie sich unter eine Lichtquelle.';
 //
 Internationalization.LifiTxt3fr = 'Lorsque vous êtes devant une borne<br/> elle vous localise et affiche automatiquement<br/> sur votre tablette les informations sur l\'oeuvre<br/> qui est devant vous.';
-Internationalization.LifiTxt3eng = '';
-Internationalization.LifiTxt3ned = '';
-Internationalization.LifiTxt3deu = '';
+Internationalization.LifiTxt3eng = 'When you are standing in front of a terminal, the light source will find you and automatically display the information about the work in front of you on your tablet.';
+Internationalization.LifiTxt3ned = 'Als u voor een terminal gaat staan, dan lokaliseert hij u en geeft hij op uw tablet automatisch informatie weer over het kunstwerk voor u.';
+Internationalization.LifiTxt3deu = 'Wenn Sie vor einer Markierung stehen, werden sie automatisch entdeckt, und auf Ihrem Tablet erscheinen die Informationen des Kunstwerkes, vor dem Sie stehen.';
 //
 Internationalization.LifiBtnNextfr = 'Suivant';
-Internationalization.LifiBtnNexteng = '';
-Internationalization.LifiBtnNextned = '';
-Internationalization.LifiBtnNextdeu = '';
+Internationalization.LifiBtnNexteng = 'Next';
+Internationalization.LifiBtnNextned = 'Volgende';
+Internationalization.LifiBtnNextdeu = 'Nächstes';
 
 /* ---- MAP ---- */
 //
 Internationalization.MapLegendfr = 'choix du niveau';
-Internationalization.MapLegendeng = '';
-Internationalization.MapLegendned = '';
-Internationalization.MapLegenddeu = '';
+Internationalization.MapLegendeng = 'Choice of level';
+Internationalization.MapLegendned = 'Keuze van het niveau';
+Internationalization.MapLegenddeu = 'Wahl der Ebene';
 //
 Internationalization.MapLvlDefLegend2fr = 'Sens de la visite';
-Internationalization.MapLvlDefLegend2eng = '';
-Internationalization.MapLvlDefLegend2ned = '';
-Internationalization.MapLvlDefLegend2deu = '';
+Internationalization.MapLvlDefLegend2eng = 'Direction of the tour';
+Internationalization.MapLvlDefLegend2ned = 'Richting van het bezoek';
+Internationalization.MapLvlDefLegend2deu = 'Richtung der Besichtigung';
 //
 Internationalization.MapLvlDefLegend3fr = 'Oeuvres<br/>non visitées';
-Internationalization.MapLvlDefLegend3eng = '';
-Internationalization.MapLvlDefLegend3ned = '';
-Internationalization.MapLvlDefLegend3deu = '';
+Internationalization.MapLvlDefLegend3eng = 'Non-visited <br/>works';
+Internationalization.MapLvlDefLegend3ned = 'Niet-bezochte kunstwerken';
+Internationalization.MapLvlDefLegend3deu = 'Nicht besichtigte Kunstwerke';
 //
 Internationalization.MapLvlDefLegend4fr = 'Oeuvres visitées';
-Internationalization.MapLvlDefLegend4eng = '';
-Internationalization.MapLvlDefLegend4ned = '';
-Internationalization.MapLvlDefLegend4deu = '';
+Internationalization.MapLvlDefLegend4eng = 'Visited works';
+Internationalization.MapLvlDefLegend4ned = 'Bezochte kunstwerken';
+Internationalization.MapLvlDefLegend4deu = 'Besichtigte Kunstwerke';
 //
 Internationalization.MapLvlDefLegend5fr = 'Oeuvre suivante';
-Internationalization.MapLvlDefLegend5eng = '';
-Internationalization.MapLvlDefLegend5ned = '';
-Internationalization.MapLvlDefLegend5deu = '';
+Internationalization.MapLvlDefLegend5eng = 'Next work';
+Internationalization.MapLvlDefLegend5ned = 'Volgend kunstwerk';
+Internationalization.MapLvlDefLegend5deu = 'Nächstes Kunstwerk';
 //
 Internationalization.MapLvl1Legend2fr = 'Accueil';
-Internationalization.MapLvl1Legend2eng = '';
-Internationalization.MapLvl1Legend2ned = '';
-Internationalization.MapLvl1Legend2deu = '';
+Internationalization.MapLvl1Legend2eng = 'Reception';
+Internationalization.MapLvl1Legend2ned = 'Onthaal';
+Internationalization.MapLvl1Legend2deu = 'Empfang';
 //
 Internationalization.MapLvl1Legend3fr = 'Vestiaires';
-Internationalization.MapLvl1Legend3eng = '';
-Internationalization.MapLvl1Legend3ned = '';
-Internationalization.MapLvl1Legend3deu = '';
+Internationalization.MapLvl1Legend3eng = 'Cloakrooms';
+Internationalization.MapLvl1Legend3ned = 'Vestiaires';
+Internationalization.MapLvl1Legend3deu = 'Umkleideräume';
 //
 Internationalization.MapLvl1Legend4fr = 'Boutique';
-Internationalization.MapLvl1Legend4eng = '';
-Internationalization.MapLvl1Legend4ned = '';
-Internationalization.MapLvl1Legend4deu = '';
+Internationalization.MapLvl1Legend4eng = 'Shop';
+Internationalization.MapLvl1Legend4ned = 'Winkel';
+Internationalization.MapLvl1Legend4deu = 'Shop';
 //
 Internationalization.MapLvl1Legend5fr = 'Caféteria';
-Internationalization.MapLvl1Legend5eng = '';
-Internationalization.MapLvl1Legend5ned = '';
-Internationalization.MapLvl1Legend5deu = '';
+Internationalization.MapLvl1Legend5eng = 'Cafeteria';
+Internationalization.MapLvl1Legend5ned = 'Cafetaria';
+Internationalization.MapLvl1Legend5deu = 'Cafeteria';
 //
 Internationalization.MapLegend1fr = 'Votre localisation';
-Internationalization.MapLegend1eng = '';
-Internationalization.MapLegend1ned = '';
-Internationalization.MapLegend1deu = '';
+Internationalization.MapLegend1eng = 'Your last location';
+Internationalization.MapLegend1ned = 'Uw locatie';
+Internationalization.MapLegend1deu = 'Ihr Standort';
 //
 Internationalization.MapLegend6fr = 'Toilettes';
-Internationalization.MapLegend6eng = '';
-Internationalization.MapLegend6ned = '';
-Internationalization.MapLegend6deu = '';
+Internationalization.MapLegend6eng = 'Toilets';
+Internationalization.MapLegend6ned = 'Toiletten';
+Internationalization.MapLegend6deu = 'Toiletten';
 //
 Internationalization.MapLegend7fr = 'Escaliers';
-Internationalization.MapLegend7eng = '';
-Internationalization.MapLegend7ned = '';
-Internationalization.MapLegend7deu = '';
+Internationalization.MapLegend7eng = 'Stairs';
+Internationalization.MapLegend7ned = 'Trappen';
+Internationalization.MapLegend7deu = 'Treppen';
 //
 Internationalization.MapLegend8fr = 'Ascenseurs';
-Internationalization.MapLegend8eng = '';
-Internationalization.MapLegend8ned = '';
-Internationalization.MapLegend8deu = '';
+Internationalization.MapLegend8eng = 'Lifts';
+Internationalization.MapLegend8ned = 'Liften';
+Internationalization.MapLegend8deu = 'Aufzüge';
 //
 Internationalization.MapLegendBlockfr = 'Ma position';
 Internationalization.MapLegendBlockeng = 'My position';
-Internationalization.MapLegendBlockned = '';
-Internationalization.MapLegendBlockdeu = '';
+Internationalization.MapLegendBlockned = 'Mijn positie';
+Internationalization.MapLegendBlockdeu = 'Meine Position';
 /* ---- Artworks ---- */
 //
 Internationalization.PlanBtnfr = 'plan';
-Internationalization.PlanBtneng = 'map';
-Internationalization.PlanBtnned = '';
-Internationalization.PlanBtndeu = '';
+Internationalization.PlanBtneng = 'Map';
+Internationalization.PlanBtnned = 'Map';
+Internationalization.PlanBtndeu = 'Map';
 //
 Internationalization.ArtItemTitlefr = 'Sélectionnez une oeuvre';
-Internationalization.ArtItemTitleeng = 'Select an artwork';
-Internationalization.ArtItemTitlened = '';
-Internationalization.ArtItemTitledeu = '';
+Internationalization.ArtItemTitleeng = 'Select a work';
+Internationalization.ArtItemTitlened = 'Selecteer een kunstwerk';
+Internationalization.ArtItemTitledeu = 'Ein Kunstwerk auswählen';
 //
 Internationalization.ArtLinkBtnfr = 'Oeuvres liées';
-Internationalization.ArtLinkBtneng = 'Linked artworks';
-Internationalization.ArtLinkBtnned = '';
-Internationalization.ArtLinkBtndeu = '';
+Internationalization.ArtLinkBtneng = 'Related works';
+Internationalization.ArtLinkBtnned = 'Verwante kunstwerken';
+Internationalization.ArtLinkBtndeu = 'Verbundenes Kunstwerk';
 // 
 /* ---- Details ---- */
 //
-Internationalization.DetailBtnTextfr = 'Détail';
-Internationalization.DetailBtnTexteng = 'Detail';
-Internationalization.DetailBtnTextned = '';
-Internationalization.DetailBtnTextdeu = '';
+Internationalization.DetailBtnTextfr = 'Détails';
+Internationalization.DetailBtnTexteng = 'Details';
+Internationalization.DetailBtnTextned = 'Detail';
+Internationalization.DetailBtnTextdeu = 'Detail';
 /* ---- Tools ---- */
 //
 Internationalization.ToolsHeadbandfr = 'Changer de langue';
 Internationalization.ToolsHeadbandeng = 'Change language';
-Internationalization.ToolsHeadbandned = '';
-Internationalization.ToolsHeadbanddeu = '';
+Internationalization.ToolsHeadbandned = 'Taal wijzigen';
+Internationalization.ToolsHeadbanddeu = 'Die Sprache auswählen';
 //
 Internationalization.ToolsChooseVisitfr = 'Choisissez votre visite';
-Internationalization.ToolsChooseVisiteng = 'Choose a visit';
-Internationalization.ToolsChooseVisitned = '';
-Internationalization.ToolsChooseVisitdeu = '';
+Internationalization.ToolsChooseVisiteng = 'Select your tour';
+Internationalization.ToolsChooseVisitned = 'Kies uw bezoek';
+Internationalization.ToolsChooseVisitdeu = 'Ihre Besichtigung auswählen';
 //
 Internationalization.ToolsChooseLangfr = 'Choisissez votre langue';
-Internationalization.ToolsChooseLangeng = 'Choose a language';
-Internationalization.ToolsChooseLangned = '';
-Internationalization.ToolsChooseLangdeu = '';
+Internationalization.ToolsChooseLangeng = 'Select your language';
+Internationalization.ToolsChooseLangned = 'Kies uw taal';
+Internationalization.ToolsChooseLangdeu = 'Ihre Sprache auswählen ';
 //
 Internationalization.ToolsTxtValidatefr = 'Êtes-vous sûr(e) de votre choix ?';
-Internationalization.ToolsTxtValidateeng = 'Are you sure about your choice ?';
-Internationalization.ToolsTxtValidatened = '';
-Internationalization.ToolsTxtValidatedeu = '';
+Internationalization.ToolsTxtValidateeng = 'Are you sure you have made the correct choice?';
+Internationalization.ToolsTxtValidatened = 'Bent u zeker van uw keuze?';
+Internationalization.ToolsTxtValidatedeu = 'Sind Sie sich Ihrer Auswahl sicher ?';
 //
 Internationalization.ToolsBtnValidatefr = 'Continuer';
 Internationalization.ToolsBtnValidateeng = 'Continue';
-Internationalization.ToolsBtnValidatened = '';
-Internationalization.ToolsBtnValidatedeu = '';
+Internationalization.ToolsBtnValidatened = 'Ga verder';
+Internationalization.ToolsBtnValidatedeu = 'Fortfahren';
 //
 Internationalization.ToolsBtnCancelfr = 'Annuler';
 Internationalization.ToolsBtnCanceleng = 'Cancel';
-Internationalization.ToolsBtnCancelned = '';
-Internationalization.ToolsBtnCanceldeu = '';
+Internationalization.ToolsBtnCancelned = 'Annuleer';
+Internationalization.ToolsBtnCanceldeu = 'Löschen';
--- js/src/content/ContentLangageView.class.js
+++ js/src/content/ContentLangageView.class.js
@@ -13,15 +13,31 @@ ContentLangageView.prototype.onClick = function(e){
 	switch(jQuery(e.currentTarget).attr('id')){
 		case 'fr':
 			Cookie.setCookie('lang.curtius.com','fr','365');
+			jQuery('html').removeClass('langENG, langNED, langDEU');
+		    jQuery('html').addClass('langFR');
+		    jQuery('.lang').parent().removeClass('paramSelected');
+		    jQuery('#fr').parent().addClass('paramSelected');
 		break;
 		case 'eng':
 			Cookie.setCookie('lang.curtius.com','eng','365');
+	        jQuery('html').removeClass('langFR, langNED, langDEU');
+	        jQuery('html').addClass('langENG');
+	        jQuery('.lang').parent().removeClass('paramSelected');
+	        jQuery('#eng').parent().addClass('paramSelected');
 		break;
 		case 'ned':
 			Cookie.setCookie('lang.curtius.com','ned','365');
+	        jQuery('html').removeClass('langFR, langENG, langDEU');
+	        jQuery('html').addClass('langNED');
+	        jQuery('.lang').parent().removeClass('paramSelected');
+	        jQuery('#ned').parent().addClass('paramSelected');
 		break;
 		case 'deu':
 			Cookie.setCookie('lang.curtius.com','deu','365');
+	        jQuery('html').removeClass('langFR, langENG, langNED');
+	        jQuery('html').addClass('langDEU');
+	        jQuery('.lang').parent().removeClass('paramSelected');
+	        jQuery('#deu').parent().addClass('paramSelected');
 		break;
 	}
 };
